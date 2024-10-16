21b3b2de2a7d2c809633d596508e2ee0f11a6603
zael T <teszael14@gmail.com>
2024-10-16 17:59:29 +0300

    minor

--- server/config/links.js
+++ server/config/links.js
@@ -1,3 +1,3 @@
-export const client="https://chat-on-snappy.netlify.app/"
-export const api = "https://chat-app-aeqj.onrender.com"
+module.exports.client="https://chat-on-snappy.netlify.app/"
+module.exports.api = "https://chat-app-aeqj.onrender.com"
 
