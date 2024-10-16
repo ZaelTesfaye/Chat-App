5ef0e1d68bd74739a20243b0f813c301f89cc0af
zael T <teszael14@gmail.com>
2024-10-16 17:01:54 +0300

    google oauth redirect url set to production

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,13 +1,14 @@
- import React, {useContext, useEffect, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {Link, useNavigate} from "react-router-dom";
 import styled from "styled-components";
 import Logo from "../assets/logo.png";
 import {toast} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"
 import axios from "axios";
-import {loginRoute, authenticationRoute} from "../config/APIRoutes";
+import {authenticationRoute, loginRoute} from "../config/APIRoutes";
 import LoginButtons from '../Components/loginButtons';
- import {AuthContext} from "../context/authContext";
+import {AuthContext} from "../context/authContext";
+import toastOptions from "../config/toastOptions";
 
 function Login() {
     const navigate = useNavigate();
@@ -33,13 +34,6 @@ function Login() {
         password: "",
     })
 
-    const toastOptions = {
-        position: "bottom-right",
-        autoClose: 8000,
-        draggable: true,
-        pauseOnHover: true,
-        theme: "dark",
-    }
 
     async function handleSubmit(event) {
         const {username, password} = formInput;
@@ -58,8 +52,7 @@ function Login() {
                 toast.error(data.message, toastOptions);
                 setIsSubmitted(false);
             }
-        }
-        else setIsSubmitted(false);
+        } else setIsSubmitted(false);
     }
 
     function handleChange(event) {
@@ -72,119 +65,119 @@ function Login() {
         )
     }
 
-        function handleValidation() {
-            setIsSubmitted(true);
-            const {username, password} = formInput;
-            if (username.length < 4) {
-                toast.error("Username should contain at least 4 characters", toastOptions);
-                return false;
-            } else if (password.length < 4) {
-                toast.error("Password should contain at least 8 characters", toastOptions);
-                return false;
-            } else
-                return true;
-        }
-
-        return (
-            <div className="registration">
-                <FormContainer>
-                    <form action="" onSubmit={handleSubmit}>
-                        <div className="brand">
-                            <img src={Logo} alt="Brand"/>
-                            <h1>Snappy</h1>
-                        </div>
-
-                        <input type="text" placeholder="Username" name="username" onChange={handleChange}/>
-                        <input type="password" placeholder="Password" name="password" onChange={handleChange}/>
-                        <button className="mainBtn" disabled={isSubmitted} type="submit">Login</button>
-                        <LoginButtons/>
-                        <span>Don't have an account? <Link to="/Register">Sign Up</Link> </span>
-                    </form>
-                </FormContainer>
-            </div>
-        )
+    function handleValidation() {
+        setIsSubmitted(true);
+        const {username, password} = formInput;
+        if (username.length < 4) {
+            toast.error("Username should contain at least 4 characters", toastOptions);
+            return false;
+        } else if (password.length < 4) {
+            toast.error("Password should contain at least 8 characters", toastOptions);
+            return false;
+        } else
+            return true;
     }
 
-    const FormContainer = styled.div`
+    return (
+        <div className="registration">
+            <FormContainer>
+                <form action="" onSubmit={handleSubmit}>
+                    <div className="brand">
+                        <img src={Logo} alt="Brand"/>
+                        <h1>Snappy</h1>
+                    </div>
+
+                    <input type="text" placeholder="Username" name="username" onChange={handleChange}/>
+                    <input type="password" placeholder="Password" name="password" onChange={handleChange}/>
+                    <button className="mainBtn" disabled={isSubmitted} type="submit">Login</button>
+                    <LoginButtons/>
+                    <span>Don't have an account? <Link to="/Register">Sign Up</Link> </span>
+                </form>
+            </FormContainer>
+        </div>
+    )
+}
+
+const FormContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: 100vw;
+    background-color: #131324;
+
+    .brand {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 1rem;
+
+        img {
+            height: 5rem;
+            width: 5rem;
+        }
+
+        h1 {
+            text-transform: capitalize;
+            font-size: 25px;
+        }
+    }
+
+    form {
         display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: center;
-        height: 100vh;
-        width: 100vw;
-        background-color: #131324;
+        gap: 2rem;
+        background-color: #00000076;
+        border-radius: 2rem;
+        padding: 3rem 5rem;
 
-        .brand {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            gap: 1rem;
+        input {
+            background-color: transparent;
+            padding: 1rem;
+            border: 0.1rem solid #4e0eff;
+            border-radius: 0.4rem;
+            color: white;
 
-            img {
-                height: 5rem;
-                width: 5rem;
-            }
-
-            h1 {
-                text-transform: capitalize;
-                font-size: 25px;
+            &:focus {
+                border: 0.1rem solid #997af0;
+                outline: none;
             }
         }
 
-        form {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            gap: 2rem;
-            background-color: #00000076;
-            border-radius: 2rem;
-            padding: 3rem 5rem;
+        .mainBtn {
+            background-color: #997af0;
+            color: white;
+            padding: 1rem 2rem;
+            border: none;
+            font-weight: bold;
+            cursor: pointer;
+            border-radius: 0.4rem;
+            font-size: 1rem;
+            text-transform: uppercase;
+            transition: 0.5s ease-in-out;
 
-            input {
-                background-color: transparent;
-                padding: 1rem;
-                border: 0.1rem solid #4e0eff;
-                border-radius: 0.4rem;
-                color: white;
+            &:hover {
+                background-color: #4e0eff;
+            }
+        }
 
-                &:focus {
-                    border: 0.1rem solid #997af0;
-                    outline: none;
-                }
+        span {
+            color: white;
+            text-transform: uppercase;
+
+            :hover {
+                color: #5031b2;
             }
 
-            .mainBtn {
-                background-color: #997af0;
-                color: white;
-                padding: 1rem 2rem;
-                border: none;
+            a {
+                color: #4e0eff;
                 font-weight: bold;
-                cursor: pointer;
-                border-radius: 0.4rem;
-                font-size: 1rem;
-                text-transform: uppercase;
-                transition: 0.5s ease-in-out;
-
-                &:hover {
-                    background-color: #4e0eff;
-                }
+                text-decoration: none;
             }
-
-            span {
-                color: white;
-                text-transform: uppercase;
-
-                :hover {
-                    color: #5031b2;
-                }
-
-                a {
-                    color: #4e0eff;
-                    font-weight: bold;
-                    text-decoration: none;
-                }
-            }
-        }`;
+        }
+    }`;
 
 export default Login;
